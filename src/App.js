5c10fec4e61d06f921c11984f4f01b9cdb04207b
Ajay Sarwade <ajaysarwade@gmail.com>
2024-08-18 11:48:32 +0530

    progress bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const timer = useRef(null);
+
+  useEffect(() => {
+    if (progress < 100) {
+      timer.current = setInterval(() => {
+        setProgress((prev) => prev + 1);
+      }, 100);
+    } else {
+      clearInterval(timer.current); // Clear the interval correctly
+      timer.current = null;
+    }
+
+    return () => {
+      clearInterval(timer.current); // Cleanup interval on component unmount
+    };
+  }, [progress]); // Include progress in the dependency array
+
+  function handleStart() {
+    if (progress < 100) {
+      timer.current = setInterval(() => {
+        setProgress((prev) => prev + 1);
+      }, 100);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progress Bar : {progress}%</h1>
+      <div className="progress-container">
+        <div className="progress" style={{ width: `${progress}%` }}></div>
+      </div>
+
+      {/* progress bar control using start stop  */}
+      <button onClick={handleStart}>start</button>
+      <button
+        onClick={(e) => {
+          clearInterval(timer.current);
+        }}
+      >
+        stop
+      </button>
     </div>
   );
 }
